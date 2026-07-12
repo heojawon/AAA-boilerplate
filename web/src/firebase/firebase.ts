ac5d6b91f908d9bd03bed13d7ebb7804166d375f
heojawon <ljhmm10@gmail.com>
2026-07-12 18:27:39 +0900

    chore: initalize firebase deploy and delete unncessary files - configure firebase deploy: initalized firebase deploy. - delete unncessary files: deleted ui codes in "components/ui".

--- web/src/firebase/firebase.ts
+++ web/src/firebase/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,4 +25,4 @@ export const db = getFirestore(app);
 
 export const auth = getAuth(app);
 
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
